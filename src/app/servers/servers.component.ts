e1fc633bfe5d9aacaf88aee11b7c6ad242da0f33
Chris Korbel <christopher.j.korbel@jpl.nasa.gov>
2019-02-06 15:42:21 -0800

    saving work

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,16 +2,30 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `  
-    <app-server></app-server>
-    <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = 'No server was created';
+  serverName: string = '';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true; 
+    }, 2000)
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'server was created ' + this.serverName;
+  }
+
+  //no longer needed with mgModel two way binding
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
   }
 
 }
