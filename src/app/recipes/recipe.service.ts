851025b56acd1b202a5f7cb999eaf0e18525da29
Christopher Korbel <ckorbel@Christophers-MacBook-Pro.local>
2019-03-02 14:48:01 -0800

    services services services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,28 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     
     private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'this is a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Another test recipe', 'this is a naother test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+        new Recipe(
+            'A test recipe', 
+            'this is a test', 
+            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', 
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 2)
+            ]),
+        new Recipe(
+            'Another test recipe', 
+            'this is a naother test', 
+            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+            [
+                new Ingredient('salsa', 1),
+                new Ingredient('ships', 100)
+            ]),
     ];
 
     getRecipes() {
